57e3b3d38ca0f1588da3e6e77c5792de206a11f9
Farjad47 <farjadahmed47@gmail.com>
2023-06-09 23:33:35 +0500

    "FEAT LINKS"

--- Assignment-5/app.js
+++ Assignment-5/app.js
@@ -94,3 +94,12 @@ for (var i=1;i<=10;i++){
     document.write(number + " x " + i + " = " + number * i + "<br>" );
 
 }
+
+/* 6. The Temperature Converter: It’s hot out! Let’s make a
+converter based on the steps here.
+a. Store a Celsius temperature into a variable.
+b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+c. Now store a Fahrenheit temperature into a variable.
+d. Convert it to Celsius & output “NNoF is NNoC”.
+Conversion Formulae: */
+
